4fe556a0c884dfce164419c2fc832ed31cc889f0
Lucas Brugnago da Maia <54671855+lbrugnagodamaia@users.noreply.github.com>
2020-02-13 21:09:29 -0300

    Update IdiomaDoSistema.java

--- src/idiomadosistema/IdiomaDoSistema.java
+++ src/idiomadosistema/IdiomaDoSistema.java
@@ -1,19 +1,14 @@
 package idiomadosistema;
 
-
 import java.util.Locale;
 
 public class IdiomaDoSistema {
 
 	public static void main(String[] args) {
 	
-	
 		Locale idioma = Locale.getDefault();
 		idioma.getDisplayLanguage();
-		System.out.println("O idioma do sistema é: " + idioma);
-		
-
+		System.out.println("O idioma do sistema Ã©: " + idioma);
 	}
-
 }
 
